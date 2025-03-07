0cee0d7d80b19a5131dd0e3461e2acc6bdfd88e7
pavel-tymoshchuk <pavel-tymoshchuk@uadreams.com>
2025-03-07 22:16:25 +0200

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+    ...compat.extends(
+        'next/core-web-vitals',
+        'next/typescript',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended'
+    ),
+    {
+        plugins: ['react', 'react-hooks', 'prettier'],
+        rules: {
+            'react/jsx-uses-react': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+]
 
-export default eslintConfig;
+export default eslintConfig
